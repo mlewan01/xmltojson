f1369eb6cfebdc9ccd488de182880aa275b9d199
maniek <le.wy@op.pl>
2019-02-26 21:17:01 +0000

    absolutely positive that this is lots of regress

--- ind2.php
+++ ind2.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 
-$myfile = fopen("short.xml", "r") or die("Unable to open file!");
+$myfile = fopen("short3.xml", "r") or die("Unable to open file!");
 $stream = fread($myfile,filesize("data.xml"));
 fclose($myfile);
 //echo $stream;
@@ -11,33 +11,59 @@ $xml = new SimpleXMLElement($stream);
 //RecurseXML($xml);
 //echo $xml.current($xml);
 //recurs($xml, 0);
-rec($xml, 0);
+rec($xml, 0); echo "<br/>";
+
+//var_dump($xml->count());
+//var_dump($xml->children());
+
+//print_r($xml->children());
+rec2($xml);
+function rec2($xml){
+    foreach($xml->children() as $key=>$val){
+        echo "\"$key\"";
+//        var_dump($key);
+//        var_dump($val);
+        if($val instanceof SimpleXMLElement) {
+            echo ":";
+        }
+    }
+}
+
+
+
+
 function rec($xml, $depth){
     $child_count = 0;
     if($xml->count() > 0){
 //        echo $xml->count();
         foreach($xml->children() as $key => $val){
             $tab = depth($depth);
-            echo "$tab $key";
-            if($val instanceof SimpleXMLElement){  echo "<br/>";}
+            echo "$tab \"$key\":";
+            if($val instanceof SimpleXMLElement){
+                if(is_string($val)){
+                    echo ">>>>>>>>>>>>>>>>>>>>>it is string<<<<<<<<<<<<<<<";
+                }
+//                var_dump($val[0]);
+                echo "{";
+                echo "<br/>";
+            }
             if($val->attributes()[0] != NULL){
-                echo "<b>DUPA </b><br/>";
+                $tab1 = depth($depth+1);
+                foreach($val->attributes() as $k2=>$v2){
+                    echo "$tab1 \"@$k2\": <b>\"$v2\"</b><br/>";
+                }
 //                var_dump($val->attributes());
             }
             $child_count++;
             if(rec($val, ++$depth) == 0){
                 print( " <b>$val</b> <br/>");
+            }else{
+//                echo "<br/>";
             }
         }
     }
     return $child_count;
 }
-
-
-
-
-
-
 
     function recurs($xml,$depth){
         $child_count=0;
