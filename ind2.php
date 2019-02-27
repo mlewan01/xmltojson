c0742cdd186c85f1d289c56153e17c3ae45a56d5
maniek <le.wy@op.pl>
2019-02-27 19:42:07 +0000

    clouds everywere

--- ind2.php
+++ ind2.php
@@ -1,26 +1,113 @@
 <?php
 error_reporting(E_ALL);
 
-$myfile = fopen("short3.xml", "r") or die("Unable to open file!");
+$myfile = fopen("short6.xml", "r") or die("Unable to open file!");
 $stream = fread($myfile,filesize("data.xml"));
 fclose($myfile);
+//$stream = str_replace(array("\r", "\n"), '', $stream);
+$stream = trim(preg_replace('/\s+/', '', $stream));
 //echo $stream;
 //$stream = readfile('data.xml');
-$xml = new SimpleXMLElement($stream);
+//$xml = new SimpleXMLElement($stream);
+$xml  = new SimpleXmlIterator($stream);
 //print_r($xml);
 //RecurseXML($xml);
 //echo $xml.current($xml);
 //recurs($xml, 0);
-rec($xml, 0); echo "<br/>";
+//rec($xml, 0); echo "<br/>";
 
 //var_dump($xml->count());
 //var_dump($xml->children());
-
+//$xml->rewind();
+//var_dump($xml->current());
+//echo $xml->key();
 //print_r($xml->children());
-rec2($xml);
+echo "{";rec4($xml);echo "}<br/><br/>";
+echo "{";
+//rec2($xml);
+rec3($xml);
+echo "}";
+function rec4($xml, $cc=0, $ss=0){
+    $child_count = 0;
+    $current = $xml->getName();
+    echo "\"$current\"";
+//    $xml-
+    $children = $xml->count();
+    echo "$children-$cc-$ss";
+    echo ":";
+    if($children > 0){
+        echo "{";
+    }
+    foreach($xml as $key=>$val){
+        $child_count++;
+        if(rec4($val, $child_count,$children) == 0){
+            echo "<b>\"$val\"</b>";
+        }
+        if($children - $child_count > 0){
+            echo ",";
+        }
+    }
+    if($children > 0){
+        echo "}";
+    }
+}
+function rec3($xml){
+    $child_count = 0;
+    $current = $xml->getName();
+    echo "\"$current\"";
+    echo ":";
+
+    $xml->rewind();
+    var_dump($xml->__toString());
+    var_dump($xml->valid());
+    var_dump($xml->hasChildren());
+    var_dump($xml->key());
+    var_dump($xml->getName());
+    var_dump($xml->count());
+
+    foreach($xml as $key=>$val){
+        $child_count++;
+//        $current = $val->getName();
+//        echo ">$current<";
+        
+        if($val instanceof SimpleXmlIterator) {
+//            $val->rewind();
+//            echo $val->__toString();
+//            var_dump($val->valid());
+//            var_dump($val->hasChildren());
+//            var_dump($val->key());
+//            var_dump($val->getName());
+//            var_dump($val->count());
+            if($val->__toString()==""){
+//                echo "{";
+//                echo "true";
+            }else{
+//                echo "false";
+//                echo "{";
+            }
+            echo "{";
+        }
+
+        if(rec3($val) == 0){
+            echo " <b>\"$val\"</b> ";
+        }
+        if($val instanceof SimpleXmlIterator) {
+
+            if($val->__toString()=="") {
+                echo "}";
+            }
+//            echo "}";
+        }
+    }
+    return $child_count;
+}
+
+
 function rec2($xml){
+    $current = $xml->getName();
+    echo "\"$current\"";
     foreach($xml->children() as $key=>$val){
-        echo "\"$key\"";
+//        echo "\"$key\"";
 //        var_dump($key);
 //        var_dump($val);
         if($val instanceof SimpleXMLElement) {
--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 //    echo 'dupa';
 //    echo $stream;
 
-    echo "<br/>";
+//    echo "<br/>";
 
-    $myfile = fopen("data.xml", "r") or die("Unable to open file!");
-    echo fread($myfile,filesize("data.xml"));
-    fclose($myfile);
+//    $myfile = fopen("data.xml", "r") or die("Unable to open file!");
+//    echo fread($myfile,filesize("data.xml"));
+//    fclose($myfile);
+    phpinfo();
+
 ?>
